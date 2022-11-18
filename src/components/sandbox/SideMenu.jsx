a8a93bed73363afd308f53a4fd14b9808a8a06d7
dz1237 <2366987785@qq.com>
2022-11-18 22:51:31 +0800

    侧边栏联动完成

--- src/components/sandbox/SideMenu.jsx
+++ src/components/sandbox/SideMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd'
-import { PieChartOutlined } from '@ant-design/icons';
+import { PieChartOutlined, UserOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom'
 import style from './index.module.css';
@@ -9,6 +9,7 @@ export default function SideMenu() {
   const location = useLocation();//左侧边栏默认高亮的
   const openKeys = ["/" + location.pathname.split("/")[1]];//左侧边栏默认展开的
   const [menu, setMenu] = useState([])
+  const { role: { rights } } = JSON.parse(localStorage.getItem("token"))
   useEffect(() => {
     axios.get("http://localhost:8000/rights?_embed=children").then(res => {
       setMenu(res.data);
@@ -29,18 +30,58 @@ export default function SideMenu() {
       type,
     };
   }
+  const checkPagePermission = (item) => {
+    // console.log(item);
+    return item.pagepermisson &&  rights.includes(item.key)
+  }
+
+  // const renderMenu = (menuList) => {
+  //   console.log(rights);
+  //   console.log(menuList);
+  //   const items = [];
+  //   const child =[];
+  //   menuList.map((item) => {
+  //     if (item.children?.length > 0 && checkPagePermission(item)) {
+  //       return items.push(
+  //         getItem(item.title, item.key, <PieChartOutlined />, renderMenu(item.children))
+  //       )
+  //     }
+  //     else {
+  //       return (
+  //         item.pagepermisson && items.push(getItem(item.title, item.key,))
+  //       )
+  //     }
+  //   })
+  //   return items
+  // }
   const renderMenu = (menuList) => {
     const items = [];
-    menuList.map((item) => {
-      if (item.children?.length > 0 && item.children.length !== 0) {
-        return items.push(
-          getItem(item.title, item.key, <PieChartOutlined />, renderMenu(item.children))
-        )
-      }
-      else {
-        return (
-          item.pagepermisson && items.push(getItem(item.title, item.key,))
-        )
+    menuList?.forEach((item) => {
+      const child = [];
+      if (checkPagePermission(item)) {
+        if (item.children?.length > 0) {
+          item.children.forEach((itemchildren) => {
+            if (checkPagePermission(itemchildren)) {
+              child.push(getItem((
+                <i style={{ fontStyle: 'normal' }}>
+                  {itemchildren.title}
+                </i>
+              ), itemchildren.key, <UserOutlined />))
+            }
+          })
+          items.push(getItem((
+            <i style={{ fontStyle: 'normal' }}>
+              {item.title}
+            </i>
+          ), item.key, <UserOutlined />, child))
+        } 
+        else {
+          items.push(getItem((
+            <i style={{ fontStyle: 'normal' }}>
+              {item.title}
+            </i>
+          ), item.key, <UserOutlined />))
+        }
       }
     })
     return items
@@ -53,7 +94,7 @@ export default function SideMenu() {
           <Menu
             theme="dark"
             mode="inline"
-           selectedKeys={location.pathname}
+            selectedKeys={location.pathname}
             defaultOpenKeys={openKeys}
             items={renderMenu(menu)}
             onClick={handleClick}
